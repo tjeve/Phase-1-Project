c3e5cd128823880dda15f9b61ec7cb566aea6e96
Terrence Eve <tijayeve@gmail.com>
2019-08-26 23:05:47 -0500

    added js

--- js/restinfo.js
+++ js/restinfo.js
@@ -2,8 +2,12 @@ $(document).ready(init)
 
 function init () {
 
-    var yelpPromise = $.get("https://api.yelp.com/v3/businesses/C8BQYgLbpRyf9ENtVAujLQ")
+    var yelpPromise = jQuery.get("https://api.yelp.com/v3/businesses/C8BQYgLbpRyf9ENtVAujLQ")
     .then(function() {
         console.log("you now have the yelp data!")
     })
 }
+
+//Questions:
+// Why is $.get() not a function?
+// Why is VSCODE spazzing and marking errors for all of my js. My guess is it has something to do with package.json?
