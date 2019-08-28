533ba9289fab6c7708f4a08349b6794ee7e64e97
Kazue Sasatani <segakazue@gmail.com>
2019-08-27 20:00:16 -0500

    fix with standardjs

--- js/searchresult.js
+++ js/searchresult.js
@@ -1,3 +1,5 @@
+/* global YELP_API_KEY */
+
 const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/'
 const yelpUrlBusinessSearch = 'https://api.yelp.com/v3/businesses/search'
 const searchResult = document.querySelector('#searchResult')
@@ -14,47 +16,46 @@ console.log(inputLocation.value)
 
 btnSearch.addEventListener('click', searchBusiness)
 
-function searchBusiness(){
-
+function searchBusiness () {
   getPositionFromIPData()
-  .then(checkResponseAndReturnJson)
-  .then(position => {
-    console.log(position)
-    let params = {
-      term: inputTerm.value,
-      limit: 50
-    }
-    if(inputLocation.value === ''){
-      params.latitude = position.latitude
-      params.longitude = position.longitude
-    }else{
-      params.location = inputLocation.value
-    }
-    console.log(params)
-    fetchYelpAPI(yelpUrlBusinessSearch, params, true)
     .then(checkResponseAndReturnJson)
-    .then(handleAPIReponse)
-    .then(completeAllImageLoading)
-    .then(function(promiseArray){
-      console.log(promiseArray)
-      Promise.allSettled(promiseArray)
-      .then(masonry)
-      .catch(function(err){
-        console.error(err.message)
-      })
+    .then(position => {
+      console.log(position)
+      const params = {
+        term: inputTerm.value,
+        limit: 50
+      }
+      if (inputLocation.value === '') {
+        params.latitude = position.latitude
+        params.longitude = position.longitude
+      } else {
+        params.location = inputLocation.value
+      }
+      console.log(params)
+      fetchYelpAPI(yelpUrlBusinessSearch, params, true)
+        .then(checkResponseAndReturnJson)
+        .then(handleAPIReponse)
+        .then(completeAllImageLoading)
+        .then(function (promiseArray) {
+          console.log(promiseArray)
+          Promise.allSettled(promiseArray)
+            .then(masonry)
+            .catch(function (err) {
+              console.error(err.message)
+            })
+        })
     })
-  })
-  .catch(err => {
-    console.error(err.message)
-  })
+    .catch(err => {
+      console.error(err.message)
+    })
 }
 
-function completeAllImageLoading(){
+function completeAllImageLoading () {
   const imgs = document.querySelectorAll('img')
-  let promiseArray = []
-  imgs.forEach(function(img){
-    const promise =  new Promise(function(resolve, reject){
-      img.addEventListener('load', function(){
+  const promiseArray = []
+  imgs.forEach(function (img) {
+    const promise = new Promise(function (resolve, reject) {
+      img.addEventListener('load', function () {
         resolve()
       })
     })
@@ -67,8 +68,8 @@ function fetchYelpAPI (yelpUrl, params, corsAnywhere) {
   const url = corsAnywhere
     ? corsAnywhereUrl + generateUrlWithParams(yelpUrl, params)
     : generateUrlWithParams(yelpUrl, params)
-    console.log(url)
-  return fetch(url, {
+  console.log(url)
+  return window.fetch(url, {
     headers: {
       Authorization: 'Bearer ' + YELP_API_KEY
     }
@@ -85,26 +86,26 @@ function checkResponseAndReturnJson (res) {
 }
 
 function handleAPIReponse (json) {
-  const image_urls = json.businesses.map(function (business) {
+  const imageUrls = json.businesses.map(function (business) {
     return `<div class="grid-item"><img src=${business.image_url}></div>`
   })
-  searchResult.innerHTML = image_urls.join('')
+  searchResult.innerHTML = imageUrls.join('')
   console.log(json.businesses)
 }
 
-function getPosition (options) {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject, options)
-  })
-}
+// function getPosition (options) {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject, options)
+//   })
+// }
 
-function getPositionFromIPData(){
-  return fetch('https://api.ipdata.co?api-key=1bf5329a7500c813f5c947083b124f22dbdd34a04647000ced91d64c')
+function getPositionFromIPData () {
+  return window.fetch('https://api.ipdata.co?api-key=1bf5329a7500c813f5c947083b124f22dbdd34a04647000ced91d64c')
 }
 
 function generateUrlWithParams (baseUrl, params) {
   const ret = []
-  for (let d in params) {
+  for (const d in params) {
     if (params[d] !== null && params[d] !== undefined) {
       ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(params[d]))
     }
@@ -112,12 +113,11 @@ function generateUrlWithParams (baseUrl, params) {
   return baseUrl + '?' + ret.join('&')
 }
 
-function masonry(){
-  const msnryArea = document.querySelector('.grid');
-  const msnry = new Masonry( msnryArea, {
+function masonry () {
+  const msnryArea = document.querySelector('.grid')
+  return new window.Masonry(msnryArea, {
     // options
     itemSelector: '.grid-item',
     columnWidth: 350
-  });
-  
+  })
 }
