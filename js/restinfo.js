072c30c53770427fc85ebfed4cbe871bfd896b76
Terrence Eve <tijayeve@gmail.com>
2019-08-29 21:54:48 -0500

    added css to restInfo

--- js/restinfo.js
+++ js/restinfo.js
@@ -1,13 +1,11 @@
 $(document).ready(init)
 
 function init () {
-  // var yelpPromise = $.get('https://api.yelp.com/v3/businesses/hNNkC569C1yVpJLDasJ4yA')
-  //   .then(function () {
-  //     console.log('you now have the yelp data!')
-  //   })
   function renderRestaurantInfo (restinfo) {
     return `<div class="rest-info">
     <div class="rest-name"><a href="${restinfo.url}">${restinfo.name}</a></div>
+    <div class="rest-rating">${restinfo.rating}/5</div>
+    <div class="rest-price">${restinfo.price}</div>
     <div class="rest-number">${restinfo.phone}</div>
     <div class="rest-address">${restinfo.location.display_address.join(' ')}</div>
     <div class="rest-distance-from-user">Restaurant distance from user goes here</div>
@@ -31,6 +29,57 @@ function init () {
     success: function (restInfo) {
       const restInfoContainer = document.getElementById('rest-info-container')
       restInfoContainer.innerHTML = renderRestaurantInfo(restInfo)
+      console.log(restInfo.coordinates)
+      mapAPI()
     }
   })
+
+  function mapAPI() {
+    $.ajax({
+      url: `https://www.google.com/maps/embed/v1/MODE?key=${GOOGLE_API_KEY}&parameters`
+      success: console.log("OMG, IT WORKED!")
+    })
+  }
+  // Google Maps
+  function initMap (restInfo) {
+    // The location of Uluru
+    var coordinates = restInfo.coordinates
+    // The map, centered at Uluru
+    var map = new google.mapgit s.Map(
+      document.getElementById('map'), { zoom: 4, center: coordinates })
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({ position: coordinates, map: map })
+  }
+  // Request for Google Map
+  // const GOOGLE_API_KEY = 'AIzaSyBKpVPcTeEAp1nDw7UigighCQJvjO62mq8'
+  
+  // let mapPromise = $.get(`https://www.google.com/maps/embed/v1/MODE?key=${GOOGLE_API_KEY}&parameters`)
+  //   .then(function () {
+  //     console.log('Hello')
+  //   })
+
+  //Place the code below into the google map api request
+  // const restMapContainer= document.getElementById('rest-map-container')
+  // restMapContainer.innerHTML=  
 }
+
+// let map = new google.maps.Map(document.getElementById('map'), {
+//   center: {lat: -34.397, lng: 150.644},
+//   zoom: 8
+// });
+
+// const directionsRequest = {
+//   origin: LatLng | String | google.maps.Place,
+//   destination: LatLng | String | google.maps.Place,
+//   travelMode: TravelMode,
+//   transitOptions: TransitOptions,
+//   drivingOptions: DrivingOptions,
+//   unitSystem: UnitSystem,
+//   waypoints[]: DirectionsWaypoint,
+//   optimizeWaypoints: Boolean,
+//   provideRouteAlternatives: Boolean,
+//   avoidFerries: Boolean,
+//   avoidHighways: Boolean,
+//   avoidTolls: Boolean,
+//   region: String
+// }
