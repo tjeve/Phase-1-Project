83121801ecac3705ace3400608c21b9ed6893e2f
Paige Ballard <p.ballardhou@gmail.com>
2019-09-03 18:51:17 -0500

    updated index.html with new links and css with new background images

--- js/layout.js
+++ js/layout.js
@@ -1,9 +1,34 @@
-
-
 $(document).ready(function(){
-    $('.header').height($(window).height());
-  })
+  $('.header').height($(window).height());
+
+  });
+  
+// $('#btn-search').on('click', function(event) {
+//     var target = $(this.getAttribute('#searchResult'));
+//     if( target.length ) {
+//         event.preventDefault();
+//         $('html, body').stop().animate({scrollTop:position}, speed, "swing");
+//     }
+  // $(function() {
+  //   var $elem = $('#searchResult');
+  //   $('#btn-search').click(
+  //     function (e) {
+  //       $('html, body').animate({scrollTop: $elem.height()}, 500);
+  //     }
+  //   )
+  // });
+  
+    
 
 
+// $(function(){
+//   $('a[href^="#"]').click(function(){
+//     let speed = 500;
+//     let href= $(this).attr("href");
+//     let target = $(href == "#" || href == "" ? 'html' : href);
+//     let position = target.offset().top;
+//     $("html, body").animate({scrollTop:position}, speed, "swing");
+//     return false;
+//   });
+// });
 
- 
