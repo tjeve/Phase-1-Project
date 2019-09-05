f441f28ebeaf8e16be06363668b557fa23a6c483
Kazue Sasatani <segakazue@gmail.com>
2019-09-04 21:36:11 -0500

    Added login user name in navbar

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,25 +1,21 @@
-$(document).ready(function(){
-  $('.header').height($(window).height());
+$(document).ready(function () {
+  $('.header').height($(window).height())
+})
 
-  });
-  
 // $('#btn-search').on('click', function(event) {
 //     var target = $(this.getAttribute('#searchResult'));
 //     if( target.length ) {
 //         event.preventDefault();
 //         $('html, body').stop().animate({scrollTop:position}, speed, "swing");
 //     }
-  // $(function() {
-  //   var $elem = $('#searchResult');
-  //   $('#btn-search').click(
-  //     function (e) {
-  //       $('html, body').animate({scrollTop: $elem.height()}, 500);
-  //     }
-  //   )
-  // });
-  
-    
-
+// $(function() {
+//   var $elem = $('#searchResult');
+//   $('#btn-search').click(
+//     function (e) {
+//       $('html, body').animate({scrollTop: $elem.height()}, 500);
+//     }
+//   )
+// });
 
 // $(function(){
 //   $('a[href^="#"]').click(function(){
@@ -32,3 +28,26 @@ $(document).ready(function(){
 //   });
 // });
 
+const loginLink = document.querySelector('#li-login')
+window.firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    loginLink.innerHTML = `
+    <button type="button" class="btn btn-outline-success" data-container="body" 
+    data-toggle="popover" data-placement="bottom" data-content="Logout">
+      <i class="fas fa-user-circle"></i>
+      ${user.providerData[0].displayName}
+    </button>`
+    loginLink.querySelector('button').addEventListener('click', function () {
+      window.firebase
+        .auth()
+        .signOut()
+        .then(function () {
+          window.location.href = '/'
+        })
+    })
+    $(loginLink.querySelector('button')).popover({
+      content: 'Logout',
+      trigger: 'hover'
+    })
+  }
+})
